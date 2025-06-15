91ae81b33cc1203514f1cb63b0d4e1f648f044d5
simanchala <simanchala.js@gmail.com>
2025-06-15 21:25:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Hi there
+        </p>
+        <a>
+          Learn React for cloud.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
